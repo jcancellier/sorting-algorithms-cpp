c497e4bdd682ffe5fe2c0434275e85a53edf87ef
jcancellier <sonicjcan@hotmail.com>
2018-08-15 18:43:26 -0700

    syntax fix

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 
 int main()
 {
-	std::vector<double> arr = {3, 2, 1, 1, 2, 3};
-	printValues("Initial Values", arr, arr.size());
-	bubbleSort(arr, arr.size());
-	printValues("After Sorting", arr, arr.size());
+    std::vector<double> arr = {3, 2, 1, 1, 2, 3};
+    printValues("Initial Values", arr, arr.size());
+    bubbleSort(arr, arr.size());
+    printValues("After Sorting", arr, arr.size());
 
     return 0;
 }
